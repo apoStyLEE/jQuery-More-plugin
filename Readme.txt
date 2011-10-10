63905757a51ae056f0ecae43df6acd3c27d401fe
apostylee <ugraskan@gmail.com>
2011-10-10 14:25:14 +0300

    v1

---
+++ Readme.txt
@@ -0,0 +1,23 @@
+﻿More.js v1 - 1,76kb
+
+====================================================== Açıklama
+Uzun yazıları girilen karakter sayısına göre yada belirtilen bir ayraca göre kısaltmaya yarar.
+
+====================================================== Kullanım
+[html]
+	<p class="more">
+		Aliquam in lorem in est venenatis consectetur et a risus. {more} Pellentesque venenatis viverra cursus. Nulla tempus nisl vel dolor venenatis accumsan. Integer in sem in risus ultricies accumsan eget vel erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur id massa leo, eu convallis orci. Integer aliquam nibh eget leo dapibus auctor. Quisque ultrices dapibus augue eget molestie. Maecenas pharetra magna rhoncus sem tincidunt a tincidunt eros pharetra.
+	</p>
+[/html]
+
+[js]
+	$(".more").more(); //veya
+	$(".more").more({character:50,splitChar:'daha'});
+[/js]
+
+====================================================== Ayarlar
+'text'          :   ' ..devamı'
+'textClass'     :   'mt',
+'character'     :   100,
+'splitChar'     :   '{more}',
+'removeHtmlTag' :   true
---
+++ more.js
@@ -0,0 +1,49 @@
+﻿(function ($) {
+    $.fn.more = function (options) {
+        var settings = {
+            'text'          :   ' ..devamı',
+            'textClass'     :   'mt',
+            'character'     :   100,
+            'splitChar'     :   '{more}',
+            'removeHtmlTag' :   true
+        };
+        var moreTextTemplate = "<span class='" + settings.textClass + "'>{0}</span>";
+        return this.each(function () {
+            var t = $(this);
+            var content = t.html();
+            var contentCache;
+            var contentLength = content.length;
+            var formatContent;
+
+            if (options) { $.extend(settings, options); }
+
+            if (settings.removeHtmlTag) {
+                contentCache = content;
+                content = t.text(); //replace(/<.*?>/g, "");
+                contentLength = content.length;
+            }
+
+            if (content.indexOf(settings.splitChar) != -1) {
+                formatContent = content.split(settings.splitChar)[0] + moreTextTemplate.replace("{0}", settings.text);
+            }
+            else if (contentLength > settings.character) {
+                formatContent = content.substr(0, settings.character) + moreTextTemplate.replace("{0}", settings.text);
+            }
+            else {
+                formatContent = content;
+            }
+            
+            t.html(formatContent);
+
+            // click
+            t.find("." + settings.textClass).live("click", function () {
+                if (settings.removeHtmlTag) {
+                    t.html(contentCache.replace(settings.splitChar, ""));
+                }
+                else {
+                    t.html(content.replace(settings.splitChar, ""));
+                }
+            });
+        });
+    };
+})(jQuery);
